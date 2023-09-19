77aea6652e3ed6c69fab7af26fb4f4f126498122
MateuszJankowski02 <twonightdragons@gmail.com>
2023-09-19 19:46:06 +0200

    Added basic supabase connection

--- app.vue
+++ app.vue
@@ -1,5 +1,20 @@
+<script setup>
+const supabase = useSupabaseClient()
+const countries = ref([])
+
+async function getCountries() {
+  const { data } = await supabase.from('countries').select()
+  countries.value = data
+}
+
+onMounted(() => {
+  getCountries()
+})
+</script>
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+  <ul class="test">
+    <li v-for="country in countries" :key="country.id">{{ country.name }}</li>
+  </ul>
 </template>
+<style lang="scss" scoped>
+</style>
