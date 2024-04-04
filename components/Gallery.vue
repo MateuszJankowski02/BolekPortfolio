e063baa3a44dff93d2aafbc9c1ee04fca1d9eddd
Mateusz Jankowski <twonightdragons@gmail.com>
2024-04-04 22:26:43 +0200

    Replaced internal SASS with external stylesheet

--- components/Gallery.vue
+++ components/Gallery.vue
@@ -3,33 +3,45 @@ const images = reactive([
   {
     "id": 1,
     "content": "Image1",
-    "scale": 0.8,
-    "opacity": 0.5
+    "scale": 0,
+    "opacity": 0
   },
   {
     "id": 2,
     "content": "Image2",
-    "scale": 0.9,
-    "opacity": 0.75
+    "scale": 0,
+    "opacity": 0
   },
   {
     "id": 3,
     "content": "Image3",
-    "scale": 1.0,
-    "opacity": 1
+    "scale": 0,
+    "opacity": 0
   },
   {
     "id": 4,
     "content": "Image4",
-    "scale": 0.9,
-    "opacity": 0.75
+    "scale": 0,
+    "opacity": 0
   },
   {
     "id": 5,
     "content": "Image5",
-    "scale": 0.8,
-    "opacity": 0.5
-  }
+    "scale": 0,
+    "opacity": 0
+  },
+  {
+    "id": 6,
+    "content": "Image6",
+    "scale": 0,
+    "opacity": 0
+  },
+  {
+    "id": 7,
+    "content": "Image7",
+    "scale": 0,
+    "opacity": 0
+  },
 ]);
 
 const isAnimating = ref(false);
@@ -39,6 +51,7 @@ const swapElements = function() {
   isAnimating.value = true;
   console.log(images);
   images.forEach((image, index) => {
+    /*
     if (index === 3 || index === 4) {
       image.scale = 0.8;
       image.opacity = 0.5;
@@ -49,12 +62,52 @@ const swapElements = function() {
       image.scale = 1.0;
       image.opacity = 1;
     }
+     */
+    if (index === 0 || index === 4){
+      image.scale = 0.9;
+      image.opacity = 0.75;
+    }else if(index === 1 || index === 3) {
+      image.scale = 1;
+      image.opacity = 1;
+    }else if(index === 2){
+      image.scale = 0.9;
+      image.opacity = 0.75;
+    }else if(images.length - 1) {
+      image.scale = 0.8;
+      image.opacity = 0.5;
+    }else{
+      image.scale = 0;
+      image.opacity = 0;
+    }
   });
   images.unshift(...images.splice(-1));
   setTimeout(() => {
     isAnimating.value = false;
   }, 500);
 };
+
+const initializeElements = function() {
+  images.forEach((image, index) => {
+    if (index === 0 || index === 4){
+      image.scale = 0.8;
+      image.opacity = 0.5;
+    }else if(index === 1 || index === 3) {
+      image.scale = 0.9;
+      image.opacity = 0.75;
+    }else if(index === 2){
+      image.scale = 1;
+      image.opacity = 1;
+    }else {
+      image.scale = 0;
+      image.opacity = 0;
+    }
+
+  });
+};
+
+onMounted(() => {
+  initializeElements()
+});
 </script>
 <template>
     <div class="section">
@@ -63,7 +116,7 @@ const swapElements = function() {
             <transition-group name="list" tag="div" class="images">
 
               <div
-                  v-for="image in images"
+                  v-for="image in images.slice(0, 5)"
                   :key="image.id"
                   class="image-placeholder-wrapper"
               >
