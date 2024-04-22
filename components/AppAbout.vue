855df3646f71d8bfe8db538d3bfa983352182cc9
Mateusz Jankowski <twonightdragons@gmail.com>
2024-04-22 09:13:07 +0200

    Changed about section font size

--- components/AppAbout.vue
+++ components/AppAbout.vue
@@ -10,12 +10,11 @@
               <NuxtImg src="aboutBG/aboutBG.jpg" class="aboutImage"/>
             </div>
             <div class="about-text">
-                <p>
-                  Welcome to the captivating world of automotive photography brought to life by passion to capture the beauty of cars.
-                  Immerse yourself in a gallery of stunning images that celebrate the timeless beauty and cutting-edge innovation of automobiles.
-                  From the sleek curves of high-performance sports cars to the rugged terrain conquered by off-road beasts,
-                  each one is a testament to the passion and precision behind the lens.
-                  Join me on this exhilarating journey and experience the artistry,
+                <p>Welcome to the captivating world of automotive photography brought to life by passion to capture the beauty of cars.</p>
+                <p>Immerse yourself in a gallery of stunning images that celebrate the timeless beauty and cutting-edge innovation of automobiles.</p>
+                <p>From the sleek curves of high-performance sports cars to the rugged terrain conquered by off-road beasts,
+                  each one is a testament to the passion and precision behind the lens.</p>
+                <p>Join me on this exhilarating journey and experience the artistry,
                   power, and elegance of automotive design like never before.
                 </p>
             </div>
